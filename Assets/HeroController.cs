795660addafd2b10b2bd3c8230ebeae68a239bf2
Ahmad Adillaumam <wvwnumam@gmail.com>
2022-11-19 19:23:12 +0700

    FPS course

---
+++ Assets/HeroController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			GameObject bulletObject = GameObject.Instantiate<GameObject>(bulletPrefab);
+			bulletObject.transform.position = this.transform.position;
+
+			BulletController bullet = bulletObject.GetComponent<BulletController>();
+			bullet.angle = this.transform.localEulerAngles.y;
+
+			bulletObject.transform.position = new Vector3(
+				this.transform.position.x + Mathf.Cos(-(bullet.angle - 90) * Mathf.Deg2Rad) * bullet.speed,
+				this.transform.position.y,
+				this.transform.position.z + Mathf.Sin(-(bullet.angle - 90) * Mathf.Deg2Rad) * bullet.speed
+			);
+		}
+	}
+}
